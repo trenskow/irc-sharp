ab8159a72e87c975ec63906a521418ac84529c7f
ktrenskow <ktrenskow@02dd911d-7451-0410-92cc-adf6c7b68777>
2009-01-11 11:30:28 +0000

    Updated compile script to use the output name ircsharp instead of the old capitalized IRC.

--- trunk/compile.sh
+++ trunk/compile.sh
@@ -11,10 +11,10 @@ then
 fi
 
 echo Compiling code...
-gmcs ./src/*.cs ./src/Users/*.cs ./src/Misc/*.cs ./src/Channels/*.cs ./src/DCC/*.cs ./src/ServerInfo/*.cs -target:library -out:./bin/IRC.dll -keyfile:./src/IRC.key -doc:./docs/IRC.xml > /dev/null
+gmcs ./src/*.cs ./src/Users/*.cs ./src/Misc/*.cs ./src/Channels/*.cs ./src/DCC/*.cs ./src/ServerInfo/*.cs -target:library -out:./bin/ircsharp.dll -keyfile:./src/IRC.key -doc:./docs/ircsharp.xml > /dev/null
 
 echo Generating monodoc documentation...
-monodocer -name:"IRC" -importslashdoc:./docs/IRC.xml -path:./docs/en -assembly:./bin/IRC.dll -pretty > /dev/null
+monodocer -name:"ircsharp" -importslashdoc:./docs/ircsharp.xml -path:./docs/en -assembly:./bin/ircsharp.dll -pretty > /dev/null
 
 echo Generating html documentation...
 monodocs2html --source ./docs/en --dest ./docs/html > /dev/null
@@ -22,12 +22,12 @@ monodocs2html --source ./docs/en --dest ./docs/html > /dev/null
 cd docs
 
 echo Generating monodoc assembly...
-mdassembler --ecma en -o IRC-docs &> /dev/null
+mdassembler --ecma en -o ircsharp-docs &> /dev/null
 
-echo "<?xml version=\"1.0\"?>" > IRC-docs.source
-echo "<monodoc>" >> IRC-docs.source
-echo "    <source provider=\"ecma\" basefile=\"IRC-docs\" path=\"classlib-IRC\"/>" >> IRC-docs.source
-echo "</monodoc>" >> IRC-docs.source
+echo "<?xml version=\"1.0\"?>" > ircsharp-docs.source
+echo "<monodoc>" >> ircsharp-docs.source
+echo "    <source provider=\"ecma\" basefile=\"ircsharp-docs\" path=\"various\"/>" >> ircsharp-docs.source
+echo "</monodoc>" >> ircsharp-docs.source
 
 rm -R en
 
@@ -38,5 +38,7 @@ echo All tasks complete
 echo
 echo ./bin contains binaries
 echo ./docs contains documentation
+echo
+echo Run install.sh to install assembly and documentation.
 echo
 echo Have fun!
